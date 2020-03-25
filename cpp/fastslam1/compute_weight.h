e1a36540c67b2487b45766df78f424c8b0237991
Jonathan Lehner <lehnerj@student.ethz.ch>
2020-03-25 05:07:00 +0100

    added docs for fastslam1

--- cpp/fastslam1/compute_weight.h
+++ cpp/fastslam1/compute_weight.h
@@ -8,6 +8,14 @@
 using namespace Eigen;
 using namespace std;
 
+/*!
+    Compute particle weight for sampling.
+    Uses compute_jacobians.
+    @param[out] particle Particle whose weight is calculated
+    @param[in]  z        vector of map features, calculated by data_associate_known
+    @param[in]  idf      vector of map features, calculated by data_associate_known, used for jacobians 
+    @param[in]  R        matrix of observation noises, metres and radians
+ */
 float compute_weight(Particle &particle, vector<Vector2f> z, vector<int> idf, Matrix2f R); 
 
 #endif
--- cpp/fastslam1/fastslam1_sim.h
+++ cpp/fastslam1/fastslam1_sim.h
@@ -16,7 +16,21 @@
 using namespace std;
 using namespace Eigen;
 
+/*!
+    Calculates the particles and their positions. Mostly calls other functions.
+    @param[out] particles  All particles
+    @param[in]  lm        list of landmark data
+    @param[in]  wp        list of waypoints, only used to compute steering.
+ */
 vector<Particle> fastslam1_sim(MatrixXf lm, MatrixXf wp);
+
+/*!
+    Makes laser lines (calculate but not used in fastslam1_sim).
+    It generates a plot with line_plot_conversion.
+    @param[out] matrix of laser lines
+    @param[in]  rb        measurements
+    @param[in]  xv        robot pose
+ */
 MatrixXf make_laser_lines(vector<Vector2f> rb, Vector3f xv);
 
 #endif //FASTSLAM2_SIM_H
