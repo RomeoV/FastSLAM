c0a461322b12e00bb10bf37f97c4de03902a311b
Phil26AT <phil.lindenberger@gmail.comD>
2020-04-17 13:52:09 +0200

    Include math.h in predict_true

--- cpp/core/predict_true.cpp
+++ cpp/core/predict_true.cpp
@@ -1,9 +1,9 @@
 #include "predict_true.h"
 #include "pi_to_pi.h"
-
+#include "math.h"
 void predict_true(Vector3d &xv, double V, double G, double WB, double dt) 
 {
 	xv(0) = xv(0) + V*dt*cos(G+xv(2));		
 	xv(1) = xv(1) + V*dt*sin(G+xv(2));
-	xv(2) = pi_to_pi(xv(2) + V*dt*sin(G)/WB);		
+	xv(2) = pi_to_pi(xv(2) + V*dt*std::sin(G)/WB);		
 }
